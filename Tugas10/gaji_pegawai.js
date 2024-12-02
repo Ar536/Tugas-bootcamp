d69494f63ef6230b41d1f0c3dc40c08236716229
Ar536 <ariqmuhammadfathin2@gmail.com>
2024-12-02 20:42:08 +0700

    menambahkan Tugas 10

---
+++ Tugas10/gaji_pegawai.js
@@ -0,0 +1,10 @@
+let gaji = prompt('Berapa gaji yang kamu peroleh?')
+if (gaji >= 20000000) {
+    alert('Tolong membayar tunjangan sebesar 3000000')
+}else if (gaji >= 15000000) {
+    alert('Tolong membayar tunjangan sebesar 2000000')
+}else if (gaji >= 10000000) {
+    alert('Tolong membayar tunjangan sebesar 1000000')
+}else {
+    alert('golongan tidak valid')
+}
---
+++ Tugas10/login.js
@@ -0,0 +1,18 @@
+let Username = prompt('Username ?')
+if (Username === "admin_nf") {
+    alert('lanjutkan')
+}else if (Username === "pegawai_nf") {
+    alert('lanjutkan')
+}else  {
+    alert('Username salah')
+}
+
+let password = prompt('password ?')
+if (password === "Digital75@NFA") {
+    alert('Selamat masuk admin')
+}else if (password === "NFA123@dts") {
+    alert('Selamat masuk pegawai')
+}else  {
+    alert('password salah')
+}
+
